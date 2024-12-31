1c73e39d5f077c861ffed94c1fe3a75d2c3ce360
hzmzk <hazim_zaki96@yahoo.com>
2024-12-31 12:42:46 +0800

    Update probability.h

--- include/probability.h
+++ include/probability.h
@@ -1,8 +1,6 @@
 #ifndef PROBABILITY
 #define PROBABILITY
 
-#include <cmath>
-
 double normal_cdf(double z);	//Normal cumulative distribution function
 
 double gamma_cdf(double a, double lambda, double x);	//Gamma cumulative distribution function
